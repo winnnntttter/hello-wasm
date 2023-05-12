6a912d142913a03a95db3cf92cbc5cecd5d8cfb8
hanwm <hanwm@chsi.com.cn>
2023-05-12 13:50:23 +0800

    c++调用自己函数

--- cpp-demo/html_template/hello.c
+++ cpp-demo/html_template/hello.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <emscripten/emscripten.h>
 
 int main(int argc, char ** argv) {
-  printf("Hello World\n");
+    printf("Hello World\n");
 }
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// 如果需要调用一个在 C 语言自定义的函数，你可以使用 Emscripten 中的 ccall() 函数，以及 EMSCRIPTEN_KEEPALIVE 声明（将你的函数添加到导出函数列表中
+// 默认情况下，Emscripten 生成的代码只会调用 main() 函数，其他的函数将被视为无用代码。在一个函数名之前添加 EMSCRIPTEN_KEEPALIVE 能够防止这样的事情发生。你需要导入 emscripten.h 库来使用 EMSCRIPTEN_KEEPALIVE。
+// 注意由于使用 ccall 函数，需要添加指定参数 emcc -o hello3.html hello3.c -O3 -s WASM=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']" --shell-file html_template/shell_minimal.html
+
+int EMSCRIPTEN_KEEPALIVE myFunction(int argc, char ** argv) {
+  printf("我的函数已被调用\n");
+}
+
+#ifdef __cplusplus
+}
+#endif
